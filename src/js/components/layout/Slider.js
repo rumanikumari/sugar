c23a567f79d1d2c558609f3a5c9ad2dca712b91c
Vipin Megotia <vipinmegotia@Vipins-MBP.Dlink>
2017-06-13 23:58:16 +0530

    Added carousel images

--- src/js/components/layout/Slider.js
+++ src/js/components/layout/Slider.js
@@ -4,23 +4,19 @@ import {Carousel} from "react-responsive-carousel";
 export default class Slider extends React.Component {
 
   render() {
-    const responsive_image = {
-      height:"10px",
-      width:"10px"
-    };
     return (
-      <Carousel axis="vertical" showArrows={false} infiniteLoop autoPlay>
+      <Carousel autoPlay interval={1000} infiniteLoop>
         <div>
-          <img src="../../../assets/pic1.jpeg" />
+          <img src="../../../assets/2.jpeg" />
           <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src="../../../assets/pic2.jpeg"/>
-          <p className="legend">Legend 2</p>
+          <img src="../../../assets/3.jpeg" />
+          <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src="../../../assets/pic3.jpeg"/>
-          <p className="legend">Legend 3</p>
+          <img src="../../../assets/4.jpeg" />
+          <p className="legend">Legend 1</p>
         </div>
       </Carousel>
     );
