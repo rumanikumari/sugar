f6da7652784110fd2194765bf784d4d062d6c1a9
Kumari <ev71@INMAC613NG.local>
2017-06-24 00:58:15 +0530

    slider

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,11 +1,33 @@
 import React from "react";
+import Slider from "../components/layout/Slider";
 
 export default class Home extends React.Component {
+
   render() {
+    const containerStyle = {
+      marginTop: "60px"
+    };
     return (
       <div>
-        <h1>Home</h1>
+        <Slider/>
+        <div class="container" style={containerStyle}>
+          <div class="row">
+            <div class="col-lg-4">
+            <p>my name is Rumani</p>
+
+            </div>
+            <div class="col-lg-4">
+            <p>my name is Vipin</p>
+
+            </div>
+            <div class="col-lg-4">
+            <p>my name is pratiksha</p>
+
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import Nav from "../components/layout/Nav";
-import Slider from "../components/layout/Slider";
 
 export default class Layout extends React.Component {
   render() {
@@ -13,15 +12,8 @@ export default class Layout extends React.Component {
     return (
       <div>
         <Nav location={location}/>
-        <Slider/>
         <div class="container" style={containerStyle}>
-          <div class="row">
-            <div class="col-lg-12">
-
-              {this.props.children}
-
-            </div>
-          </div>
+        {this.props.children}
         </div>
       </div>
     );
