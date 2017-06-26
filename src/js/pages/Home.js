008f9da5fa319ed6cb5629105e895883b27cfd76
Kumari <ev71@INMAC613NG.Dlink>
2017-06-26 21:53:59 +0530

    smaller image

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -14,12 +14,14 @@ export default class Home extends React.Component {
     return (
     <div>
         <Slider/>
-        <div class="image">
+        <div class="containerStyle">
+        <p>Every year Bajaj Hindusthan Sugar ltd. conducts a survey of its command area to bring on record the cane cultivated area of the farmers. This also takes care of the different varieties of sugarcane that are grown by farmers.
+Post this massive exercise, every farmer within the command area of the mill is provided with a calendar, which tells him when he can expect a Mill Supply Ticket (Purchy) against which he will supply the cane</p>
+        </div>
+        <div class="image img-responsive">
             <img src="../../assets/sugar.png" alt="" class="blur"/>
              <p class="textOnImage">A Movie in the Park: Kung Fu Panda</p>
              </div>
-
-
    </div>
     );
   }
