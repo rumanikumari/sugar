964ece071cf06d60875f46529cf49bd77a44bce6
Vipin Megotia <vipinmegotia@Vipins-MBP.Dlink>
2017-06-26 21:53:06 +0530

    Created a calss

---
+++ src/js/components/layout/ImageCommonDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default class ImageCommonDiv extends React.Component {
+    render() {
+      return (
+        <div class="row">
+          <div class="col-lg-12">
+           <div class="panel panel-primary">
+           </div>
+          </div>
+      </div>
+      );
+    }
+}
