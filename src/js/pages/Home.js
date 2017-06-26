603e2fc3c8ca6938b0c44335079a84cc44e406b0
Vipin Megotia <vipinmegotia@Vipins-MBP.Dlink>
2017-06-26 23:46:27 +0530

    Added slider text

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -19,8 +19,8 @@ export default class Home extends React.Component {
 Post this massive exercise, every farmer within the command area of the mill is provided with a calendar, which tells him when he can expect a Mill Supply Ticket (Purchy) against which he will supply the cane</p>
         </div>
         <div class="image img-responsive">
-            <img src="../../assets/sugar.png" alt="" class="blur"/>
-             <p class="textOnImage">A Movie in the Park: Kung Fu Panda</p>
+            <img src="../../assets/combinedimage.jpg" alt="" class="blur"/>
+             <div class="textOnImage">A Movie in the Park: Kung Fu Panda</div>
              </div>
    </div>
     );
