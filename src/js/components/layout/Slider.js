05db8ace3a75add3451a0a9e2af766e0163aa591
Vipin Megotia <vipinmegotia@Vipins-MacBook-Pro.local>
2017-11-07 22:12:26 +0530

    Added some UI changes

--- src/js/components/layout/Slider.js
+++ src/js/components/layout/Slider.js
@@ -6,13 +6,12 @@ export default class Slider extends React.Component {
   render() {
 
     return (
-      <Carousel showThumbs={false}
-  showStatus={false}
-  useKeyboardArrows
-  className="presentation-mode" infiniteLoop>
+      <Carousel showThumbs={false} showStatus={false} useKeyboardArrows className="presentation-mode" infiniteLoop>
         <div>
-          <img src="../../../assets/industry.jpg" class="slider-image-1"/>
-          <p className="text-on-slider">Our comapnay is great. It has a great history.</p>
+          <img src="../../../assets/industry.jpg" className="slider-image-1"/>
+          <p className="text-on-slider img-responsive">Our company is great. It has a great history. It is the best rated industry. It fulfills all your need.<br/>
+          </p>
+          <div class="clear"></div>
         </div>
         <div>
           <img src="../../../assets/3.png" />
